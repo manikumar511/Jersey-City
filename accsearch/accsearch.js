f905e4adcb8e61959899861eb5f5b67b6735f9d2
manikumar511 <64631719+manikumar511@users.noreply.github.com>
2022-12-24 14:03:39 -0500

    Update accsearch.js

--- accsearch/accsearch.js
+++ accsearch/accsearch.js
@@ -2,7 +2,7 @@ import { LightningElement,track } from 'lwc';
 
 export default class Accsearch extends LightningElement {
     @track name1
-    name='actor rajinikanth tamil';
+    name='actor rajinikanth tamil abc';
     changename(event){
         console.log('event.target.value======='+event.target.value);
 		console.log('event.target.value======='+event.target.value);
